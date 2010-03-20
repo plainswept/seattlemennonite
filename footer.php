3de1e27fbea0576669c5286b6bb06fcc0222e21e
Matt Yoder <yomatters@gmail.com>
2010-03-20 12:20:34 -0700

    Initial commit.

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Seattle Mennonite
+ */
+?>
+
+        <div class="clear"></div>
+   	</div>
+	<div id="footer">
+	    <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+	    <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>
+	    <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</div>
+		
+	<?php wp_footer(); ?>
+	
+</div>
+</body>
+
+</html>
