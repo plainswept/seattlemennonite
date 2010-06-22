f1a08b226c547663b5a095f766c05b6391e253f0
Matt Yoder <yomatters@gmail.com>
2010-06-21 22:18:10 -0700

    Styled page title.

--- page.php
+++ page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 <div id="content">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="post" id="post-<?php the_ID(); ?>">
-		<h2><?php the_title(); ?></h2>
+		<h2 class="page-title"><?php the_title(); ?></h2>
 		<?php the_content('<p>Read the rest of this page &raquo;</p>'); ?>
 		<?php wp_link_pages(array('before' => '<p>Pages: ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 	</div>
--- single.php
+++ single.php
@@ -10,7 +10,7 @@ get_header();
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<h2><?php the_title(); ?></h2>
+			<h2 class="post-title"><?php the_title(); ?></h2>
 			<p class="post-byline">
 			    <?php the_time('F jS, Y') ?>
 			    &mdash; <?php the_author() ?>
